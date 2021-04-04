3aca596732af60a8a7760f5a7cab93861b55ef8b
seyed <mohammadjavadsabagh@gmail.com>
2021-04-04 13:21:30 +0430

    add slider git

--- app/src/main/java/com/example/koochita/MainActivity.java
+++ app/src/main/java/com/example/koochita/MainActivity.java
@@ -1,24 +1,48 @@
 package com.example.koochita;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.transition.AutoTransition;
+import android.transition.TransitionManager;
 import android.view.View;
-import android.widget.Toolbar;
+import android.widget.Button;
 
-import com.nightonke.jellytogglebutton.JellyToggleButton;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        ConstraintLayout expandableView;
+        Button arrowBtn;
+        CardView cardView;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        JellyToggleButton toggleButton;
-//        toggleButton = findViewById(R.id.test);
-//        toggleButton.setLeftText("tes1");
-//        toggleButton.setRightText("22s");
+                //Slider Item                //Slider Item                //Slider Item
+        expandableView = findViewById(R.id.expandableView);
+        arrowBtn = findViewById(R.id.arrowBtn);
+        cardView = findViewById(R.id.cardView);
+
+        arrowBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (expandableView.getVisibility() == View.GONE) {
+                    TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
+                    expandableView.setVisibility(View.VISIBLE);
+//                    arrowBtn.setBackgroundResource(R.drawable.ic_keyboard_arrow_up_black_24dp);
+                } else {
+                    TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
+                    expandableView.setVisibility(View.GONE);
+//                    arrowBtn.setBackgroundResource(R.drawable.ic_keyboard_arrow_down_black_24dp);
+                }
+            }
+        });
+
+        //Slider Item                //Slider Item                //Slider Item
 
     }
 }
