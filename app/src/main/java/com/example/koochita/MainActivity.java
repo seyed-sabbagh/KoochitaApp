17dc7d8999fbeec6a9eaaac1ca5588a298908fb2
seyed <mohammadjavadsabagh@gmail.com>
2021-04-04 12:59:11 +0430

    ToolBar ImageView TextView

---
+++ app/src/main/java/com/example/koochita/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.koochita;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toolbar;
+
+import com.nightonke.jellytogglebutton.JellyToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        JellyToggleButton toggleButton;
+//        toggleButton = findViewById(R.id.test);
+//        toggleButton.setLeftText("tes1");
+//        toggleButton.setRightText("22s");
+
+    }
+}
