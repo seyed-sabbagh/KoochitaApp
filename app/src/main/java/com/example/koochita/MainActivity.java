16ea8112fcf49d295a28bbc74cf970745c05ae33
seyed <mohammadjavadsabagh@gmail.com>
2021-04-06 15:09:13 +0430

    clean include to MainActivity

--- app/src/main/java/com/example/koochita/MainActivity.java
+++ app/src/main/java/com/example/koochita/MainActivity.java
@@ -22,33 +22,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        ConstraintLayout expandableView;
-        Button arrowBtn;
-        CardView cardView;
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //Slider Item                //Slider Item                //Slider Item
-        expandableView = findViewById(R.id.expandableView);
-        arrowBtn = findViewById(R.id.arrowBtn);
-        cardView = findViewById(R.id.cardView);
 
-        arrowBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (expandableView.getVisibility() == View.GONE) {
-                    TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
-                    expandableView.setVisibility(View.VISIBLE);
-//                    arrowBtn.setBackgroundResource(R.drawable.ic_keyboard_arrow_up_black_24dp);
-                } else {
-                    TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
-                    expandableView.setVisibility(View.GONE);
-//                    arrowBtn.setBackgroundResource(R.drawable.ic_keyboard_arrow_down_black_24dp);
-                }
-            }
-        });
 
         //Slider Item                //Slider Item                //Slider Item
 
