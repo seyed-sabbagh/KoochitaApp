d4d9abe9a48c7247c30b5fe7df65aede9824d452
seyed <mohammadjavadsabagh@gmail.com>
2021-04-07 15:33:26 +0430

    bottomSheetContainer

--- app/src/main/java/com/example/koochita/ModiriatOtaghActivity.java
+++ app/src/main/java/com/example/koochita/ModiriatOtaghActivity.java
@@ -1,14 +1,53 @@
 package com.example.koochita;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class ModiriatOtaghActivity extends AppCompatActivity {
+
+    Button btnShow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_modiriat_otagh);
+
+
+
+
+        btnShow = findViewById(R.id.button_elane_takhfif);
+
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(
+                        ModiriatOtaghActivity.this, R.style.BottomSheetDialogTheme);
+                @SuppressLint("WrongViewCast") View bottomSheetView = LayoutInflater.from(getApplicationContext())
+                        .inflate(
+                                R.layout.layout_bottom_sheet,
+                                (ConstraintLayout)findViewById(R.id.bottomSheetContainer)
+                        );
+                bottomSheetView.findViewById(R.id.bottomSheetContainer).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        bottomSheetDialog.dismiss();
+                    }
+                });
+                bottomSheetDialog.setContentView(bottomSheetView);
+                bottomSheetDialog.show();
+            }
+        });
+
     }
 }
