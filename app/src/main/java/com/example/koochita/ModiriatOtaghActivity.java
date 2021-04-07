47071218eb6dba1dcfd4cd2d6eb08ce2b905e901
seyed <mohammadjavadsabagh@gmail.com>
2021-04-07 10:53:42 +0430

    activity_modiriat_otagh.xml  Finish

---
+++ app/src/main/java/com/example/koochita/ModiriatOtaghActivity.java
@@ -0,0 +1,14 @@
+package com.example.koochita;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModiriatOtaghActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modiriat_otagh);
+    }
+}
