e2651b515dba7d6cd32ddf1a07860c59baa565b6
seyed <mohammadjavadsabagh@gmail.com>
2021-04-08 14:58:47 +0430

    activity_elam_mosafer.xml Main finish

---
+++ app/src/main/java/com/example/koochita/ElamMosaferActivity.java
@@ -0,0 +1,17 @@
+package com.example.koochita;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ElamMosaferActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_elam_mosafer);
+
+
+
+    }
+}
