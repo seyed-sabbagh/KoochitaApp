4a9a575c0570a2a701a508811b3fbc406d68bbcc
seyed <mohammadjavadsabagh@gmail.com>
2021-04-10 12:07:15 +0430

    layout_bottom_sheet_joziyat_mosafer.xml finish

--- app/src/main/java/com/example/koochita/ElamMosaferActivity.java
+++ app/src/main/java/com/example/koochita/ElamMosaferActivity.java
@@ -1,17 +1,44 @@
 package com.example.koochita;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class ElamMosaferActivity extends AppCompatActivity {
+
+    Button btn;
+    ConstraintLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_elam_mosafer);
 
+        btn = findViewById(R.id.btn_elame_mosafer);
+        layout = findViewById(R.id.bottomSheetContainer2);
 
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(
+                        ElamMosaferActivity.this,
+                        R.style.BottomSheetDialogTheme);
+                View bottomSheetView = LayoutInflater.from(getApplicationContext())
+                        .inflate(
+                                R.layout.layout_bottom_sheet_joziyat_mosafer,
+                                (ConstraintLayout) findViewById(R.id.bottomSheetContainer2)
+                        );
+
+                bottomSheetDialog.setContentView(bottomSheetView);
+                bottomSheetDialog.show();
+            }
+        });
 
     }
 }
